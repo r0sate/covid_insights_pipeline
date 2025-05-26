7040aaa5b3e0ee86b540d556e94827791b85dacc
r0sate <lucas.rosate@hotmail.com>
2025-05-26 01:58:19 -0300

    update: rm target from the project

--- target/compiled/covid_insights_pipeline/models/marts/dimensions/dim_location_type.sql
+++
@@ -1,6 +0,0 @@
-SELECT *
-FROM (
-    VALUES
-        (0, 'Country'),
-        (1, 'Aggregated Region')
-) AS t(location_type, description)
--- target/compiled/covid_insights_pipeline/tests/test_missing_dates_per_country.sql
+++
@@ -1,6 +0,0 @@
-SELECT location_id
-FROM COVID_DB.dbt_FACT.fct_covid_metrics
-GROUP BY location_id
-HAVING COUNT(DISTINCT date) < (
-    SELECT COUNT(*) FROM COVID_DB.dbt_DIM.dim_date
-)
